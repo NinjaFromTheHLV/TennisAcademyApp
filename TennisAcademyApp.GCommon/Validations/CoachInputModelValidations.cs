22bfe8fa3d664673be97de6be40f1a38203d14fa
Yordan Pavlov <iordan_pavlov1@abv.bg>
2025-07-21 10:36:50 +0300

    Validations

---
+++ TennisAcademyApp.GCommon/Validations/CoachInputModelValidations.cs
@@ -0,0 +1,16 @@
+﻿namespace TennisAcademyApp.ViewModels.Validations
+{
+    public static class CoachInputModelValidations
+    {
+        public const string RequiredNameMessage = "Name is required.";
+        public const string NameMinLengthMessage = "Name must be at least 5 characters.";
+        public const string NameMaxLengthMessage = "Name cannot exceed 50 characters.";
+
+        public const string AgeRequiredMessage = "Age is required.";
+        public const string AgeErrorMessage = "Age must be between 18 and 75!";
+
+        public const string DescriptionRequiredMessage = "Description is required.";
+        public const string DescriptionMinLengthMessage = "Description must be at least 10 characters.";
+        public const string DescriptionMaxLengthMessage = "Description cannot exceed 150 characters.";
+    }
+}
---
+++ TennisAcademyApp.GCommon/Validations/ValidationConstants.cs
@@ -0,0 +1,21 @@
+﻿namespace TennisAcademyApp.GCommon.Validations
+{
+    public static class ValidationConstants
+    {
+        public static class Coach
+        {
+            public const int CoachNameMinLenght = 5;
+            public const int CoachNameMaxLenght = 50;
+
+            public const int CoachAgeMinLenght = 18;
+            public const int CoachAgeMaxLenght = 75;
+
+            public const int CoachDescriptionMinLenght = 10;
+            public const int CoachDescriptionMaxLenght = 150;
+        }
+        public static class Reservation
+        {
+            public const int PlayerNotesMaxLenght = 70;
+        }
+    }
+}
