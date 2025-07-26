8b0fbd1a8b6ed40c470a11507bb50b2c4608271c
Yordan Pavlov <iordan_pavlov1@abv.bg>
2025-07-26 13:31:09 +0300

    Reservation Details and Delete logic done

--- TennisAcademyApp/Controllers/ReservationController.cs
+++ TennisAcademyApp/Controllers/ReservationController.cs
@@ -1,9 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using TennisAcademyApp.Services.Core.Contracts;
 using TennisAcademyApp.ViewModels.Reservation;
-using static TennisAcademyApp.GCommon.Validations.ErrorMessages.Reservation;
-using System.Globalization;
-using Microsoft.AspNetCore.Mvc.ModelBinding;
 
 namespace TennisAcademyApp.Controllers
 {
@@ -31,6 +28,10 @@ namespace TennisAcademyApp.Controllers
             try
             {
                 string? userId = GetUserId();
+                if (userId == null)
+                {
+                    return RedirectToAction(nameof(Index), "Home");
+                }
 
                 var reservations = await reservationService.GetUserReservationsAsync(userId);
 
@@ -47,11 +48,7 @@ namespace TennisAcademyApp.Controllers
         {
             try
             {
-                string? userId = GetUserId();
-                if (userId == null)
-                {
-                    return RedirectToAction(nameof(Index), "Home");
-                }
+                string userId = GetUserId()!;
 
                 var model = new ReservationCreateInputModel
                 {
@@ -77,11 +74,7 @@ namespace TennisAcademyApp.Controllers
                 {
                     return View(model);
                 }
-                string? userId = GetUserId();
-                if (userId == null)
-                {
-                    return RedirectToAction(nameof(Index), "Home");
-                }
+                string userId = GetUserId()!;
 
                 bool isCreated = await reservationService.CreateReservationAsync(userId, model);
                 if (isCreated == false)
@@ -98,6 +91,62 @@ namespace TennisAcademyApp.Controllers
                 model.Surfaces = await surfaceService.GetSurfacesForDropDownAsync();
                 model.TrainingTypes = await trainingTypeService.GetAllTrainingTypesForDropDownAsync();
                 return View(model);
+            }
+        }
+        [HttpGet]
+        public async Task<IActionResult> Details(int? id)
+        {
+            try
+            {
+                string userId = GetUserId()!;
+
+                var reservationDetails = await reservationService.GetUserReservationDetailsAsync(userId, id);
+
+                return View(reservationDetails);
+            }
+            catch (ArgumentException ex)
+            {
+                ModelState.AddModelError(string.Empty, ex.Message);
+                return RedirectToAction(nameof(Index), "Home");
+            }
+        }
+        [HttpGet]
+        public async Task<IActionResult> Delete(int? id)
+        {
+            try
+            {
+                string userId = GetUserId()!;
+
+                var reservationToDelete = await reservationService.GetUserReservationForDeletingAsync(userId, id);
+
+                return View(reservationToDelete);
+            }
+            catch (ArgumentException ex)
+            {
+                ModelState.AddModelError(string.Empty, ex.Message);
+                return RedirectToAction(nameof(Index), "Home");
+            }
+        }
+        [HttpPost]
+        public async Task<IActionResult> Delete(ReservationDeleteViewModel model)
+        {
+            try
+            {
+                string userId = GetUserId()!;
+
+                bool isDeleted = await reservationService.DeleteReservationAsync(userId, model);
+                if (isDeleted == false)
+                {
+                    ModelState.AddModelError(string.Empty, "Failed to delete the reservation. Please try again.");
+                    return View(model);
+                }
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch (ArgumentException ex)
+            {
+                ModelState.AddModelError(string.Empty, ex.Message);
+                return RedirectToAction(nameof(Index), "Home");
             }
         }
     }
