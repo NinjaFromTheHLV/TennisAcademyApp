78a92b85089bcce19fef146f78f40b8566b4a7f1
Yordan Pavlov <iordan_pavlov1@abv.bg>
2025-08-01 19:50:57 +0300

    Racket ViewModels added

---
+++ TennisAcademyApp.ViewModels/Racket/RacketCreateInputModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using static TennisAcademyApp.GCommon.Validations.RequiredMessages.Racket;
+using static TennisAcademyApp.GCommon.Validations.ErrorMessages.Racket;
+
+namespace TennisAcademyApp.ViewModels.Racket
+{
+    public class RacketCreateInputModel
+    {
+        [Required(ErrorMessage = BrandRequiredErrorMessage)]
+        public string Brand { get; set; } = null!;
+        [Required(ErrorMessage = ModelRequiredErrorMessage)]
+        public string Model { get; set; } = null!;
+        [Required(ErrorMessage = PriceRequiredErrorMessage)]
+        [Range(30.00, 1500.00, ErrorMessage = PriceRangeErrorMessage)]
+        public decimal Price { get; set; }
+        [Required(ErrorMessage = QuantityRequiredErrorMessage)]
+        public int Quantity { get; set; }
+        [Required(ErrorMessage = ImageUrlRequiredErrorMessage)]
+        public string ImageUrl { get; set; } = null!;
+    }
+}
