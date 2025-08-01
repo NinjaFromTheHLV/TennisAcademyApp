9d89f2a43d0b951e4e0672592df8f3e9e8cede02
Yordan Pavlov <iordan_pavlov1@abv.bg>
2025-08-01 19:45:02 +0300

    new entities added

---
+++ TennisAcademy.Data.Models/Racket.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace TennisAcademyApp.Data.Models
+{
+    [Comment("Rackets Shop")]
+    public class Racket
+    {
+        [Key]
+        [Comment("Racket Identifier")]
+        public int Id { get; set; }
+        [Required]
+        [Comment("Racket Brand")]
+        public string Brand { get; set; } = null!;
+        [Required]
+        [Comment("Racket Model")]
+        public string Model { get; set; } = null!;
+        [Required]
+        [Comment("Racket Price")]
+        public decimal Price { get; set; }
+        [Required]
+        [Comment("Available in stock")]
+        public int Quantity { get; set; }
+        [Required]
+        [Comment("Racket Image")]
+        public string ImageUrl { get; set; } = null!;
+        public virtual ICollection<RacketCart> RacketCart { get; set; } = new HashSet<RacketCart>();
+    }
+}
---
+++ TennisAcademy.Data.Models/RacketCart.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace TennisAcademyApp.Data.Models
+{
+    [Comment("Racket Cart")]
+    public class RacketCart
+    {
+        [Key]
+        [Comment("Racket Cart Identifier")]
+        public int Id { get; set; }
+        [Required]
+        [Comment("Foreign Key of Racket")]
+        public int RacketId { get; set; }
+        public virtual Racket Racket { get; set; } = null!;
+        [Required]
+        [Comment("Foreign Key of IdentityUser")]
+        public string UserId { get; set; } = null!;
+        public virtual IdentityUser User { get; set; } = null!;
+        [Required]
+        [Comment("Quantity of Rackets in Cart")]
+        public int Quantity { get; set; }
+    }
+}
