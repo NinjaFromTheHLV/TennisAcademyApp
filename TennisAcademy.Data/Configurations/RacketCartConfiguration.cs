fce6b1b60972d047e4b700198ecded1bfc337022
Yordan Pavlov <iordan_pavlov1@abv.bg>
2025-08-01 19:45:58 +0300

    Configured new entites

---
+++ TennisAcademy.Data/Configurations/RacketCartConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using TennisAcademyApp.Data.Models;
+
+namespace TennisAcademyApp.Data.Configurations
+{
+    public class RacketCartConfiguration : IEntityTypeConfiguration<RacketCart>
+    {
+        public void Configure(EntityTypeBuilder<RacketCart> config)
+        {
+            config
+                .HasOne(rc => rc.Racket)
+                .WithMany(r => r.RacketCart)
+                .HasForeignKey(rc => rc.RacketId);
+
+            config
+                .HasOne(rc => rc.User)
+                .WithMany()
+                .HasForeignKey(rc => rc.UserId);
+        }
+    }
+}
