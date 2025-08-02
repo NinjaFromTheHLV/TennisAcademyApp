a1b0293214afb2d687bc892017fd92a966001961
Yordan Pavlov <iordan_pavlov1@abv.bg>
2025-08-02 12:54:26 +0300

    Racket cart index view

---
+++ TennisAcademyApp.ViewModels/Cart/RacketCartViewModel.cs
@@ -0,0 +1,9 @@
+﻿using TennisAcademyApp.ViewModels.Racket;
+
+namespace TennisAcademyApp.ViewModels.Cart
+{
+    public class RacketCartViewModel : RacketIndexViewModel
+    {
+        public decimal TotalPrice { get; set; }
+    }
+}
