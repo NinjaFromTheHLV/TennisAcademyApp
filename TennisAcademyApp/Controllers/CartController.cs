f2f5ce576f674165a4ce6dbdbd66ed94b45ab74f
Yordan Pavlov <iordan_pavlov1@abv.bg>
2025-08-02 12:53:22 +0300

    CartController added

---
+++ TennisAcademyApp/Controllers/CartController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TennisAcademyApp.Services.Core.Contracts;
+
+namespace TennisAcademyApp.Controllers
+{
+    public class CartController : BaseController
+    {
+        private readonly ICartService cartService;
+        public CartController(ICartService cartService)
+        {
+            this.cartService = cartService;
+        }
+        public async Task<IActionResult> RacketsIndex()
+        {
+            try
+            {
+                string userId = GetUserId()!;
+                var racketsInCart = await this.cartService.GetAllRacketsInCartAsync(userId);
+                return View(racketsInCart);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error retrieving rackets in cart: {ex.Message}");
+                return RedirectToAction(nameof(Index), "Home");
+            }
+        }
+        [HttpPost]
+        public async Task<IActionResult> AddRacketToCart(int racketid, int quantity)
+        {
+            try
+            {
+                string userId = GetUserId()!;
+                await this.cartService.AddRacketToCartAsync(userId, racketid, quantity);
+                return RedirectToAction(nameof(RacketsIndex));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error adding racket to cart: {ex.Message}");
+                return RedirectToAction(nameof(Index), "Home");
+            }
+        }
+        [HttpPost]
+        public async Task<IActionResult> RemoveRacketFromCart(int id, int racketId)
+        {
+            try
+            {
+                string userId = GetUserId()!;
+                bool result = await cartService.RemoveRacketFromCartAsync(userId, id, racketId);
+                if (result)
+                {
+                    return RedirectToAction(nameof(RacketsIndex));
+                }
+                else
+                {
+                    return BadRequest("Failed to remove racket from cart.");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error removing racket from cart: {ex.Message}");
+                return RedirectToAction(nameof(Index), "Home");
+            }
+        }
+    }
+}
