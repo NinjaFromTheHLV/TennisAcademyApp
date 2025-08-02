a6bde01024b97bb90a336508526f4d6f69704205
Yordan Pavlov <iordan_pavlov1@abv.bg>
2025-08-02 20:59:17 +0300

    Ball and BallCart logic implemented

---
+++ TennisAcademyApp.Services.Core/BallCartService.cs
@@ -0,0 +1,122 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using TennisAcademyApp.Data;
+using TennisAcademyApp.Data.Models;
+using TennisAcademyApp.Services.Core.Contracts;
+using TennisAcademyApp.ViewModels.Cart;
+using static TennisAcademyApp.GCommon.Validations.ErrorMessages.BallCart;
+
+namespace TennisAcademyApp.Services.Core
+{
+    public class BallCartService : IBallCartService
+    {
+        private readonly TennisAcademyDbContext dbContext;
+        private readonly UserManager<IdentityUser> userManager;
+
+        public BallCartService(TennisAcademyDbContext dbContext, UserManager<IdentityUser> userManager)
+        {
+            this.dbContext = dbContext;
+            this.userManager = userManager;
+        }
+
+        public async Task<IEnumerable<BallCartIndexViewModel>> GetAllBallsInCartAsync(string userId)
+        {
+            var user = await userManager.FindByIdAsync(userId);
+
+            var ballsInCart = await dbContext.BallCart
+                .Include(bc => bc.Ball)
+                .Where(bc => bc.UserId == userId)
+                .Select(bc => new BallCartIndexViewModel
+                {
+                    Id = bc.BallId,
+                    Brand = bc.Ball.Brand,
+                    Model = bc.Ball.Model,
+                    Price = bc.Ball.Price,
+                    Quantity = bc.Quantity,
+                    TotalPrice = bc.Quantity * bc.Ball.Price,
+                    ImageUrl = bc.Ball.ImageUrl
+                })
+                .ToListAsync();
+
+            return ballsInCart;
+        }
+
+        public async Task<bool> AddBallToCartAsync(string userId, int ballId, int quantity)
+        {
+            bool result = false;
+            var ball = await dbContext.Balls.FindAsync(ballId);
+            if (ball == null || quantity <= 0 || quantity > ball.Quantity)
+            {
+                throw new InvalidOperationException(InvalidQuantityErrorMessage);
+            }
+
+            var existingItem = await dbContext.BallCart
+                .FirstOrDefaultAsync(bc => bc.UserId == userId && bc.BallId == ballId);
+
+            if (existingItem != null)
+            {
+                existingItem.Quantity += quantity;
+                ball.Quantity -= quantity;
+
+                result = true;
+            }
+            else
+            {
+                var cartItem = new BallCart
+                {
+                    UserId = userId,
+                    BallId = ballId,
+                    Quantity = quantity
+                };
+                ball.Quantity -= quantity;
+
+                await dbContext.BallCart.AddAsync(cartItem);
+                result = true;
+            }
+
+            await dbContext.SaveChangesAsync();
+
+            return result;
+        }
+
+        public async Task<bool> RemoveBallFromCartAsync(string userId, int ballId)
+        {
+            bool result = false;
+            var ball = await dbContext.Balls.FindAsync(ballId);
+
+            var cartItem = await dbContext.BallCart
+                .FirstOrDefaultAsync(bc => bc.BallId == ballId && bc.UserId == userId);
+
+            if (cartItem == null)
+            {
+                return false;
+            }
+
+            dbContext.BallCart.Remove(cartItem);
+
+            await dbContext.SaveChangesAsync();
+            result = true;
+
+            return result;
+        }
+
+        public async Task<bool> CheckOutAllBallsAsync(string userId)
+        {
+            bool result = false;
+            var user = await userManager.FindByIdAsync(userId);
+
+            var cartItems = await dbContext.BallCart
+                .Where(bc => bc.UserId == userId)
+                .ToListAsync();
+
+            if (cartItems.Any())
+            {
+                dbContext.BallCart.RemoveRange(cartItems);
+                await dbContext.SaveChangesAsync();
+
+                result = true;
+            }
+            return result;
+        }
+    }
+}
---
+++ TennisAcademyApp.Services.Core/BallService.cs
@@ -0,0 +1,161 @@
+﻿using Microsoft.AspNetCore.Identity;
+using TennisAcademyApp.Data;
+using TennisAcademyApp.Services.Core.Contracts;
+using TennisAcademyApp.ViewModels.Ball;
+using TennisAcademyApp.Data.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace TennisAcademyApp.Services.Core
+{
+    public class BallService : IBallService
+    {
+        private readonly TennisAcademyDbContext dbContext;
+        private readonly UserManager<IdentityUser> userManager;
+
+        public BallService(TennisAcademyDbContext dbContext, UserManager<IdentityUser> userManager)
+        {
+            this.dbContext = dbContext;
+            this.userManager = userManager;
+        }
+
+        public async Task<IEnumerable<BallIndexViewModel>> GetAllBallsAsync()
+        {
+            var balls = await dbContext.Balls
+                .AsNoTracking()
+                .Include(b => b.RacketCarts)
+                .Select(b => new BallIndexViewModel
+                {
+                    Id = b.Id,
+                    Brand = b.Brand,
+                    Model = b.Model,
+                    Price = b.Price,
+                    Quantity = b.Quantity,
+                    ImageUrl = b.ImageUrl,
+                })
+                .ToListAsync();
+
+            return balls;
+        }
+
+        public async Task<Ball> FindBallByIdAsync(int? id)
+        {
+            if (id.HasValue)
+            {
+                var ball = await dbContext.Balls
+                    .AsNoTracking()
+                    .FirstOrDefaultAsync(b => b.Id == id.Value);
+
+                if (ball == null)
+                {
+                    throw new ArgumentException("Ball not found.");
+                }
+
+                return ball;
+            }
+            else
+            {
+                throw new ArgumentException("Ball ID cannot be null.");
+            }
+        }
+
+        public async Task<bool> AddBallAsync(string userId, BallCreateInputModel model)
+        {
+            bool result = false;
+            var user = await userManager.FindByIdAsync(userId);
+
+            var ball = new Ball
+            {
+                Brand = model.Brand,
+                Model = model.Model,
+                Price = model.Price,
+                Quantity = model.Quantity,
+                ImageUrl = model.ImageUrl
+            };
+
+            await dbContext.AddAsync(ball);
+            await dbContext.SaveChangesAsync();
+
+            result = true;
+            return result;
+        }
+
+        public async Task<BallEditFormModel> GetBallForEditingAsync(string userId, int? id)
+        {
+            BallEditFormModel? model = null;
+            var user = await userManager.FindByIdAsync(userId);
+
+            var ball = await FindBallByIdAsync(id);
+
+            model = new BallEditFormModel
+            {
+                Id = ball.Id,
+                Brand = ball.Brand,
+                Model = ball.Model,
+                Price = ball.Price,
+                Quantity = ball.Quantity,
+                ImageUrl = ball.ImageUrl
+            };
+
+            return model;
+        }
+
+        public async Task<bool> EditBallAsync(BallEditFormModel model)
+        {
+            bool result = false;
+            var ball = await dbContext.Balls.FindAsync(model.Id);
+
+            if (ball == null)
+            {
+                return false;
+            }
+
+            ball.Brand = model.Brand;
+            ball.Model = model.Model;
+            ball.Price = model.Price;
+            ball.Quantity = model.Quantity;
+            ball.ImageUrl = model.ImageUrl;
+
+            await dbContext.SaveChangesAsync();
+
+            result = true;
+            return result;
+        }
+
+        public async Task<BallDeleteViewModel> GetBallForDeletingAsync(string userId, int? id)
+        {
+            BallDeleteViewModel? model = null;
+            var user = await userManager.FindByIdAsync(userId);
+
+            var ball = await FindBallByIdAsync(id);
+
+            model = new BallDeleteViewModel
+            {
+                Id = ball.Id,
+                Brand = ball.Brand,
+                Model = ball.Model,
+                ImageUrl = ball.ImageUrl
+            };
+
+            return model;
+        }
+
+        public async Task<bool> DeleteBallAsync(string userId, BallDeleteViewModel model)
+        {
+            bool result = false;
+            var user = await userManager.FindByIdAsync(userId);
+
+            var ball = await dbContext.Balls.FindAsync(model.Id);
+
+            if (ball == null)
+            {
+                throw new ArgumentException("Ball not found.");
+            }
+
+            dbContext.Balls.Remove(ball);
+            await dbContext.SaveChangesAsync();
+            result = true;
+
+            return result;
+        }
+    }
+}
