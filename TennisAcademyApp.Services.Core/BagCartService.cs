ade824be2fdd2c7478162fb39411a7d6f4dff01c
Yordan Pavlov <iordan_pavlov1@abv.bg>
2025-08-03 17:46:49 +0300

    Bussiness logic implemented

---
+++ TennisAcademyApp.Services.Core/BagCartService.cs
@@ -0,0 +1,115 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using TennisAcademyApp.Data;
+using TennisAcademyApp.Data.Models;
+using TennisAcademyApp.Services.Core.Contracts;
+using TennisAcademyApp.ViewModels.Cart;
+
+namespace TennisAcademyApp.Services.Core
+{
+    public class BagCartService : IBagCartService
+    {
+        private readonly TennisAcademyDbContext dbContext;
+        private readonly UserManager<IdentityUser> userManager;
+
+        public BagCartService(TennisAcademyDbContext dbContext, UserManager<IdentityUser> userManager)
+        {
+            this.dbContext = dbContext;
+            this.userManager = userManager;
+        }
+
+        public async Task<IEnumerable<BagCartIndexViewModel>> GetAllBagsInCartAsync(string userId)
+        {
+            var user = await userManager.FindByIdAsync(userId);
+
+            var bagsInCart = await dbContext.BagCart
+                .Include(bc => bc.Bag)
+                .Where(bc => bc.UserId == userId)
+                .Select(bc => new BagCartIndexViewModel
+                {
+                    Id = bc.BagId,
+                    Brand = bc.Bag.Brand,
+                    Model = bc.Bag.Model,
+                    Price = bc.Bag.Price,
+                    Quantity = bc.Quantity,
+                    TotalPrice = bc.Quantity * bc.Bag.Price,
+                    ImageUrl = bc.Bag.ImageUrl
+                })
+                .ToListAsync();
+
+            return bagsInCart;
+        }
+
+        public async Task<bool> AddBagToCartAsync(string userId, int bagId, int quantity)
+        {
+            bool result = false;
+            var bag = await dbContext.Bags.FindAsync(bagId);
+            if (bag == null || quantity <= 0 || quantity > bag.Quantity)
+            {
+                throw new InvalidOperationException("Invalid quantity or bag not found.");
+            }
+
+            var existingItem = await dbContext.BagCart
+                .FirstOrDefaultAsync(bc => bc.UserId == userId && bc.BagId == bagId);
+
+            if (existingItem != null)
+            {
+                existingItem.Quantity += quantity;
+                bag.Quantity -= quantity;
+                result = true;
+            }
+            else
+            {
+                var cartItem = new BagCart
+                {
+                    UserId = userId,
+                    BagId = bagId,
+                    Quantity = quantity
+                };
+                bag.Quantity -= quantity;
+
+                await dbContext.BagCart.AddAsync(cartItem);
+                result = true;
+            }
+
+            await dbContext.SaveChangesAsync();
+
+            return result;
+        }
+
+        public async Task<bool> RemoveBagFromCartAsync(string userId, int bagId)
+        {
+            bool result = false;
+            var cartItem = await dbContext.BagCart
+                .FirstOrDefaultAsync(bc => bc.BagId == bagId && bc.UserId == userId);
+
+            if (cartItem == null)
+            {
+                return false;
+            }
+
+            dbContext.BagCart.Remove(cartItem);
+            await dbContext.SaveChangesAsync();
+
+            result = true;
+            return result;
+        }
+        public async Task<bool> CheckOutAllBagsAsync(string userId)
+        {
+            bool result = false;
+
+            var cartItems = await dbContext.BagCart
+                .Where(bc => bc.UserId == userId)
+                .ToListAsync();
+
+            if (cartItems.Any())
+            {
+                dbContext.BagCart.RemoveRange(cartItems);
+                await dbContext.SaveChangesAsync();
+                result = true;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ TennisAcademyApp.Services.Core/BagService.cs
@@ -0,0 +1,162 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using TennisAcademyApp.Data;
+using TennisAcademyApp.Data.Models;
+using TennisAcademyApp.Services.Core.Contracts;
+using TennisAcademyApp.ViewModels.Bag;
+using static TennisAcademyApp.GCommon.Validations.ErrorMessages.Bag;
+
+namespace TennisAcademyApp.Services.Core
+{
+    public class BagService : IBagService
+    {
+        private readonly TennisAcademyDbContext dbContext;
+        private readonly UserManager<IdentityUser> userManager;
+
+        public BagService(TennisAcademyDbContext dbContext, UserManager<IdentityUser> userManager)
+        {
+            this.dbContext = dbContext;
+            this.userManager = userManager;
+        }
+
+        public async Task<IEnumerable<BagIndexViewModel>> GetAllBagsAsync()
+        {
+            var bags = await dbContext.Bags
+                .AsNoTracking()
+                .Include(b => b.BagCarts)
+                .Select(b => new BagIndexViewModel
+                {
+                    Id = b.Id,
+                    Brand = b.Brand,
+                    Model = b.Model,
+                    Price = b.Price,
+                    Quantity = b.Quantity,
+                    ImageUrl = b.ImageUrl,
+                })
+                .ToListAsync();
+
+            return bags;
+        }
+
+        public async Task<Bag> FindBagByIdAsync(int? id)
+        {
+            if (id.HasValue)
+            {
+                var bag = await dbContext.Bags
+                    .AsNoTracking()
+                    .FirstOrDefaultAsync(b => b.Id == id.Value);
+
+                if (bag == null)
+                {
+                    throw new ArgumentException(BagNotFoundErrorMessage);
+                }
+
+                return bag;
+            }
+            else
+            {
+                throw new ArgumentException(BagCannotBeNullErrorMessage);
+            }
+        }
+
+        public async Task<bool> AddBagAsync(string userId, BagCreateInputModel model)
+        {
+            bool result = false;
+            var user = await userManager.FindByIdAsync(userId);
+
+            var bag = new Bag
+            {
+                Brand = model.Brand,
+                Model = model.Model,
+                Price = model.Price,
+                Quantity = model.Quantity,
+                ImageUrl = model.ImageUrl
+            };
+
+            await dbContext.AddAsync(bag);
+            await dbContext.SaveChangesAsync();
+
+            result = true;
+            return result;
+        }
+
+        public async Task<BagEditFormModel> GetBagForEditingAsync(string userId, int? id)
+        {
+            BagEditFormModel? model = null;
+            var user = await userManager.FindByIdAsync(userId);
+
+            var bag = await FindBagByIdAsync(id);
+
+            model = new BagEditFormModel
+            {
+                Id = bag.Id,
+                Brand = bag.Brand,
+                Model = bag.Model,
+                Price = bag.Price,
+                Quantity = bag.Quantity,
+                ImageUrl = bag.ImageUrl
+            };
+
+            return model;
+        }
+
+        public async Task<bool> EditBagAsync(BagEditFormModel model)
+        {
+            bool result = false;
+            var bag = await dbContext.Bags.FindAsync(model.Id);
+
+            if (bag == null)
+            {
+                throw new ArgumentException(BagNotFoundErrorMessage);
+            }
+
+            bag.Brand = model.Brand;
+            bag.Model = model.Model;
+            bag.Price = model.Price;
+            bag.Quantity = model.Quantity;
+            bag.ImageUrl = model.ImageUrl;
+
+            await dbContext.SaveChangesAsync();
+
+            result = true;
+            return result;
+        }
+
+        public async Task<BagDeleteViewModel> GetBagForDeletingAsync(string userId, int? id)
+        {
+            BagDeleteViewModel? model = null;
+            var user = await userManager.FindByIdAsync(userId);
+
+            var bag = await FindBagByIdAsync(id);
+
+            model = new BagDeleteViewModel
+            {
+                Id = bag.Id,
+                Brand = bag.Brand,
+                Model = bag.Model,
+                ImageUrl = bag.ImageUrl
+            };
+
+            return model;
+        }
+
+        public async Task<bool> DeleteBagAsync(string userId, BagDeleteViewModel model)
+        {
+            bool result = false;
+            var user = await userManager.FindByIdAsync(userId);
+
+            var bag = await dbContext.Bags.FindAsync(model.Id);
+
+            if (bag == null)
+            {
+                throw new ArgumentException(BagNotFoundErrorMessage);
+            }
+
+            dbContext.Bags.Remove(bag);
+            await dbContext.SaveChangesAsync();
+            result = true;
+
+            return result;
+        }
+    }
+}
