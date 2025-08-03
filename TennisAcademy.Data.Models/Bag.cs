93806e6a7944e500abf849dd674c5b2986da6802
Yordan Pavlov <iordan_pavlov1@abv.bg>
2025-08-03 17:44:07 +0300

    New entities

---
+++ TennisAcademy.Data.Models/Bag.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace TennisAcademyApp.Data.Models
+{
+    [Comment("Bags Shop")]
+    public class Bag
+    {
+        [Key]
+        [Comment("Bag Identifier")]
+        public int Id { get; set; }
+
+        [Required]
+        [Comment("Bag Brand")]
+        public string Brand { get; set; } = null!;
+
+        [Required]
+        [Comment("Bag Model")]
+        public string Model { get; set; } = null!;
+
+        [Required]
+        [Comment("Bag Price")]
+        public decimal Price { get; set; }
+
+        [Required]
+        [Comment("Available in stock")]
+        public int Quantity { get; set; }
+
+        [Required]
+        [Comment("Bag Image")]
+        public string ImageUrl { get; set; } = null!;
+        public virtual ICollection<BagCart> BagCarts { get; set; } = new HashSet<BagCart>();
+    }
+}
---
+++ TennisAcademy.Data.Models/BagCart.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace TennisAcademyApp.Data.Models
+{
+    public class BagCart
+    {
+        [Required]
+        [Comment("Foreign Key of Bag")]
+        public int BagId { get; set; }
+        public virtual Bag Bag { get; set; } = null!;
+
+        [Required]
+        [Comment("Foreign Key of IdentityUser")]
+        public string UserId { get; set; } = null!;
+        public virtual IdentityUser User { get; set; } = null!;
+
+        [Required]
+        [Comment("Quantity of Bags in Cart")]
+        public int Quantity { get; set; }
+    }
+}
