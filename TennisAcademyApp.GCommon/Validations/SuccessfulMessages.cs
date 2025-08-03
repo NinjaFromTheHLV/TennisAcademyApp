ef26fa8b82c4697eed72d409e92bcc610df5ae31
Yordan Pavlov <iordan_pavlov1@abv.bg>
2025-08-03 18:37:07 +0300

    Added class successful messages

---
+++ TennisAcademyApp.GCommon/Validations/SuccessfulMessages.cs
@@ -0,0 +1,12 @@
+﻿namespace TennisAcademyApp.GCommon.Validations
+{
+    public static class SuccessfulMessages
+    {
+        public static class Coach
+        {
+            public const string CoachAddedSuccessfully = "Coach added successfully.";
+            public const string CoachUpdatedSuccessfully = "Coach updated successfully.";
+            public const string CoachDeletedSuccessfully = "Coach deleted successfully.";
+        }
+    }
+}
