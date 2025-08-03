8744a680f24979bc64b8bfc467e882fee88b34ac
Yordan Pavlov <iordan_pavlov1@abv.bg>
2025-08-03 17:47:57 +0300

    ViewModels for Bag and BagCart

---
+++ TennisAcademyApp.ViewModels/Bag/BagCreateInputModel.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using static TennisAcademyApp.GCommon.Validations.RequiredMessages.Bag;
+using static TennisAcademyApp.GCommon.Validations.ErrorMessages.Bag;
+
+namespace TennisAcademyApp.ViewModels.Bag
+{
+    public class BagCreateInputModel
+    {
+        [Required(ErrorMessage = BrandRequiredErrorMessage)]
+        public string Brand { get; set; } = null!;
+
+        [Required(ErrorMessage = ModelRequiredErrorMessage)]
+        public string Model { get; set; } = null!;
+
+        [Required(ErrorMessage = PriceRequiredErrorMessage)]
+        [Range(50.00, 1000.00, ErrorMessage = PriceRangeErrorMessage)]
+        public decimal Price { get; set; }
+
+        [Required(ErrorMessage = QuantityRequiredErrorMessage)]
+        [Range(1, int.MaxValue, ErrorMessage = QuantityErrorMessage)]
+        public int Quantity { get; set; }
+
+        [Required(ErrorMessage = ImageUrlRequiredErrorMessage)]
+        public string ImageUrl { get; set; } = null!;
+    }
+}
---
+++ TennisAcademyApp.ViewModels/Cart/BagCartIndexViewModel.cs
@@ -0,0 +1,9 @@
+﻿using TennisAcademyApp.ViewModels.Bag;
+
+namespace TennisAcademyApp.ViewModels.Cart
+{
+    public class BagCartIndexViewModel : BagIndexViewModel
+    {
+        public decimal TotalPrice { get; set; }
+    }
+}
