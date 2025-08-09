a0daa50dc15d65f2083a59db99e10d4d43ada79d
Yordan Pavlov <iordan_pavlov1@abv.bg>
2025-08-09 12:24:28 +0300

    Added a new service for user management

---
+++ TennisAcademyApp.Services.Core/Contracts/IUserService.cs
@@ -0,0 +1,11 @@
+﻿using TennisAcademyApp.ViewModels.Admin.UserManagement;
+
+namespace TennisAcademyApp.Services.Core.Contracts
+{
+    public interface IUserService
+    {
+        Task<IEnumerable<UserIndexViewModel>> GetUserManagementDataAsync(string userId);
+        //Task<IEnumerable<string>> GetManagerEmailsAsync();
+        //Task<bool> AssignUserToRoleAsync(RoleSelectionInputModel inputModel);
+    }
+}
---
+++ TennisAcademyApp.Services.Core/UserService.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using TennisAcademyApp.Services.Core.Contracts;
+using TennisAcademyApp.ViewModels.Admin.UserManagement;
+
+namespace TennisAcademyApp.Services.Core
+{
+    public class UserService : IUserService
+    {
+        private readonly UserManager<IdentityUser> userManager;
+        public UserService(UserManager<IdentityUser> userManager)
+        {
+            this.userManager = userManager;
+        }
+        public async Task<IEnumerable<UserIndexViewModel>> GetUserManagementDataAsync(string userId)
+        {
+            var usersList = await userManager.Users
+            .Where(u => u.Id != userId)
+            .ToListAsync();
+
+            var result = new List<UserIndexViewModel>();
+
+            foreach (var u in usersList)
+            {
+                var roles = await userManager.GetRolesAsync(u);
+
+                result.Add(new UserIndexViewModel
+                {
+                    Id = u.Id,
+                    Email = u.Email,
+                    Roles = roles
+                });
+            }
+
+            return result;
+        }
+    }
+}
