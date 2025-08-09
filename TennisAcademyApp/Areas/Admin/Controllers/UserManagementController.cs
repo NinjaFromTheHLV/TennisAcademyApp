0adce8b43dbcaa179fec2bd33894105cffb834ba
Yordan Pavlov <iordan_pavlov1@abv.bg>
2025-08-09 12:22:49 +0300

    Added admin controllers

---
+++ TennisAcademyApp/Areas/Admin/Controllers/UserManagementController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TennisAcademyApp.Services.Core.Contracts;
+
+namespace TennisAcademyApp.Areas.Admin.Controllers
+{
+    public class UserManagementController : AdminBaseController
+    {
+        private readonly IUserService userService;
+        public UserManagementController(IUserService userService)
+        {
+            this.userService = userService;
+        }
+        public async Task<IActionResult> Index()
+        {
+            try
+            {
+                var users = await userService.GetUserManagementDataAsync(GetUserId()!);
+
+                return View(users);
+            }
+            catch (Exception ex)
+            {
+                return RedirectToAction(nameof(Index), "Home");
+            }
+        }
+    }
+}
