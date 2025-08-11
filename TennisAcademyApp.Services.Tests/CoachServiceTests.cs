5928b08b9e8499016c86455352dcab8fa52da78b
Yordan Pavlov <iordan_pavlov1@abv.bg>
2025-08-11 12:06:19 +0300

    New project for unit tests

---
+++ TennisAcademyApp.Services.Tests/CoachServiceTests.cs
@@ -0,0 +1,24 @@
+﻿using Moq;
+using NUnit.Framework;
+using TennisAcademyApp.Services.Core.Contracts;
+using Assert = NUnit.Framework.Assert;
+
+namespace TennisAcademyApp.Services.Tests
+{
+    [TestFixture]
+    public class CoachServiceTests
+    {
+        private Mock<ICoachService> coachServiceMock;
+
+        [SetUp]
+        public void SetUp()
+        {
+            coachServiceMock = new Mock<ICoachService>();
+        }
+        [Test]
+        public void PassAlways()
+        {
+            Assert.Pass();
+        }
+    }
+}
